8f4db373401ef1b04870f8e3cca669d8a60ef801
Colby Haskell <cchaskel@usc.edu>
2022-04-09 18:03:42 -0700

    not sure

--- main.c
+++ main.c
@@ -8,5 +8,8 @@
 int main(void)
 {
     ds1631_init();
+    unsigned char buf[1] = {DS1631_CONFIG_1SHOT};
+    ds1631_set_config(buf);
+    ds1631_start_convert();
     int temp = ds1631_read_temp();
 }
