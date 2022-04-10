a494259e913e1d17ff851d72e60ffe706d628c71
Joshua Choe <choejd@usc.edu>
2022-04-10 08:38:36 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 #include <string.h>
 
 #include "ds1631.h"
+#include "lcd.h"
+
+
+const unsigned char str1[] PROGMEM = ">> at328-5.c hi <<901234";
+
+const unsigned char str2[] PROGMEM = ">> USC EE459L <<78901234";
+
 
 int main(void)
 {
@@ -12,4 +19,21 @@ int main(void)
     ds1631_set_config(buf);
     ds1631_start_convert();
     int temp = ds1631_read_temp();
+    
+    // lcd 
+    lcd_init();                 // Initialize the LCD display
+
+    lcd_moveto(0, 0);
+    lcd_stringout(str1);        // Print string on line 1
+
+    lcd_moveto(1, 0);
+    lcd_stringout(str2);        // Print string on line 2
+
+    while (1) {                 // Loop forever
+    }
+
+    
+
+    return 0;   /* never reached */
+    
 }
