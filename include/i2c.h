4ca2e1bd4cf579b1d7c3fcf3613f488524a72896
Joshua Choe <choejd@usc.edu>
2022-04-14 16:07:14 -0700

    Update i2c.h

--- include/i2c.h
+++ include/i2c.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-#define FOSC = 9830400; // Clock frequency = Oscillator freq.
-#define BDIV = (FOSC / 100000 - 16) / 2 + 1;
+#define FOSC  9830400 // Clock frequency = Oscillator freq.
+#define BDIV  (FOSC / 100000 - 16) / 2 + 1
 
 extern void i2c_init(uint8_t);
 extern uint8_t i2c_io(uint8_t, uint8_t *, uint16_t,
