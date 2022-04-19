77c696c7d3d347eaa06ddc59ce6e9cee065d3766
Joshua Choe <choejd@usc.edu>
2022-04-19 10:41:07 -0700

    Add files via upload

---
+++ include/bh1750.h
@@ -0,0 +1,52 @@
+#ifndef BH1750_H
+#define BH1750_H
+
+#include <stdio.h>
+#include <string.h>
+#include "i2c.h"
+
+#define BH1750_DEBUG 0
+
+#define BH1750_DEVICE_ADDR 0x23
+
+// No active state
+#define BH1750_POWER_DOWN 0x00
+
+// Wating for measurment command
+#define BH1750_POWER_ON 0x01
+
+// Reset data register value - not accepted in POWER_DOWN mode
+#define BH1750_RESET 0x07
+
+// Start measurement at 1lx resolution. Measurement time is approx 120ms.
+#define BH1750_CONTINUOUS_HIGH_RES_MODE  0x10
+
+// Start measurement at 0.5lx resolution. Measurement time is approx 120ms.
+#define BH1750_CONTINUOUS_HIGH_RES_MODE_2  0x11
+
+// Start measurement at 4lx resolution. Measurement time is approx 16ms.
+#define BH1750_CONTINUOUS_LOW_RES_MODE  0x13
+
+// Start measurement at 1lx resolution. Measurement time is approx 120ms.
+// Device is automatically set to Power Down after measurement.
+#define BH1750_ONE_TIME_HIGH_RES_MODE  0x20
+
+// Start measurement at 0.5lx resolution. Measurement time is approx 120ms.
+// Device is automatically set to Power Down after measurement.
+#define BH1750_ONE_TIME_HIGH_RES_MODE_2  0x21
+
+// Start measurement at 1lx resolution. Measurement time is approx 120ms.
+// Device is automatically set to Power Down after measurement.
+#define BH1750_ONE_TIME_LOW_RES_MODE  0x23
+
+#define OSTR_SIZE   80
+
+void bh1750_init(void);
+void bh1750_readLight(unsigned char* light);
+
+
+
+
+
+
+#endif
---
+++ include/pcf8563.h
@@ -0,0 +1,51 @@
+
+
+#ifndef PCF8563_H
+#define PCF8563_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include "i2c.h"
+
+#define DEVICE_ADDR 0xA2
+
+// REG ADDR
+#define PCF8563_address   0x51
+#define Control_status_1  0x00
+#define Control_status_2  0x01
+#define VL_seconds        0x02
+#define Minutes           0x03
+#define Hours             0x04
+#define Days              0x05
+#define Weekdays          0x06
+#define Century_months    0x07
+#define Years             0x08
+#define Minute_alarm      0x09
+#define Hour_alarm        0x0A
+#define Day_alarm         0x0B
+#define Weekday_alarm     0x0C
+#define CLKOUT_control    0x0D
+#define Timer_control     0x0E
+#define Timer             0x0F
+
+#define OSTR_SIZE   80
+
+// Functions
+
+void pcf8563Init(void);
+void startClock(void);
+void stopClock(void);
+void setYear(uint8_t year);
+void setMonth(uint8_t month);
+void setDay(uint8_t day);
+void setHour(uint8_t hour);
+void setMinut(uint8_t minut);
+void setSecond(uint8_t second);
+void getTime(unsigned char *rbuf);
+unsigned char bcd_to_number(uint8_t first, uint8_t second);
+uint8_t get_first_number(unsigned short number);
+uint8_t get_second_number(unsigned short number);
+
+
+#endif
