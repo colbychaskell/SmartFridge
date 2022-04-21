4e5c4a91ab85b9846b3ff08a8deb004197bccef9
Joshua Choe <choejd@usc.edu>
2022-04-20 20:50:55 -0700

    Add files via upload

--- src/ds1631.c
+++ src/ds1631.c
@@ -1,16 +1,18 @@
 #include "ds1631.h"
 
-//Initialize Sensor by setting config to default 
+// Initialize Sensor by setting config to default
 //(Power-up state: 100011XX (XX = user defined))
-// XX: POL and 1SHOT bits => we set to 10
-void ds1631_init() {
-    unsigned char buf[1] = {DS1631_CONFIG_DEFAULT};
+//  XX: POL and 1SHOT bits => we set to 10
+void ds1631_init()
+{
+    unsigned char buf[1] = {DS1631_CONFIG_1SHOT};
     ds1631_set_config(buf);
 }
 /*
     Updates config register to specified byte
 */
-void ds1631_set_config(const unsigned char* config) {
+void ds1631_set_config(const unsigned char *config)
+{
     uint8_t wbuf[1] = {DS1631_ACCESS_CONFIG}; // config register address
     unsigned char config2[1];
     config2[0] = ds1631_get_config();
@@ -18,7 +20,7 @@ void ds1631_set_config(const unsigned char* config) {
     i2c_io(DS1631_ID, wbuf, 1, config2, 1, NULL, 0); // Send config to sensor configuration register
 }
 
-unsigned char ds1631_get_config() 
+unsigned char ds1631_get_config()
 {
     unsigned char wbuf[1] = {DS1631_ACCESS_CONFIG}; // write the DS1631 configuration byte
     unsigned char rbuf[1];
@@ -30,14 +32,17 @@ unsigned char ds1631_get_config()
 /*
     Sends specified byte as command to DS1631 w/ no read
 */
-void ds1631_write_command(const unsigned char command, unsigned char* data) {
-    if (data == NULL){
+void ds1631_write_command(const unsigned char command, unsigned char *data)
+{
+    if (data == NULL)
+    {
         unsigned char wbuf[1] = {command};
         i2c_io(DS1631_ID, wbuf, 1, NULL, 0, NULL, 0);
     }
-    else{
-         unsigned char wbuf[1] = {command};
-         i2c_io(DS1631_ID, wbuf, 1, data, 2, NULL, 0);
+    else
+    {
+        unsigned char wbuf[1] = {command};
+        i2c_io(DS1631_ID, wbuf, 1, data, 2, NULL, 0);
     }
 }
 
@@ -45,21 +50,16 @@ void ds1631_write_command(const unsigned char command, unsigned char* data) {
     Sends specified byte as command to DS1631 w/ 2 bytes read back
 */
 
-
-void ds1631_read_command(const unsigned char command, unsigned char* rbuf)
+void ds1631_read_command(const unsigned char command, unsigned char *rbuf)
 {
     unsigned char rbuf2[2];
-   
 
-    char ostr[OSTR_SIZE];           // Buffer for creating strings
+    char ostr[OSTR_SIZE]; // Buffer for creating strings
 
     unsigned char wbuf[1] = {command};
     i2c_io(DS1631_ID, wbuf, 1, NULL, 0, rbuf2, 2);
     snprintf(ostr, OSTR_SIZE, "Temp: %d%d", rbuf2[0], rbuf2[1]);
-    // char temp1 = rbuf2[0] + '0';
-    // char temp2 = rbuf2[1] + '0';
-    // rbuf[0] = temp1;
-    // rbuf[1] = temp2;
+
     rbuf[0] = ostr[0];
     rbuf[1] = ostr[1];
     rbuf[2] = ostr[2];
@@ -68,21 +68,14 @@ void ds1631_read_command(const unsigned char command, unsigned char* rbuf)
     rbuf[5] = ostr[5];
     rbuf[6] = ostr[6];
     rbuf[7] = ostr[7];
-    
-    
-    
-    //rbuf[8] = ostr[8];
-    
-    
-    //rbuf[0] = 'h';
-    //return rbuf;
 }
 
 /*
     Start a temperature conversion
     Note: Only necessary while in oneShotMode
 */
-void ds1631_start_convert() {
+void ds1631_start_convert()
+{
     ds1631_write_command(DS1631_START_CONV, NULL);
 }
 void ds1631StopConvert()
@@ -90,32 +83,46 @@ void ds1631StopConvert()
     ds1631_write_command(DS1631_STOP_CONV, NULL);
 }
 
-void ds1631SetTH()
+void ds1631SetTH(uint8_t temp)
 {
-    unsigned char temp[2];
-    temp[0]= 0;
-    temp[1] = 0;
-    ds1631_write_command(DS1631_SET_TH, temp);
-}
-
-
-void ds1631ReadTH()
-{
-    // unsigned char temp[1];
-    // unsigned char rbuf[2];
-    // temp[0]= 0x40;
+    // unsigned char temp[2];
+    // temp[0] = 0;
+    // temp[1] = 0;
     // ds1631_write_command(DS1631_SET_TH, temp);
+    
+    // set the 16 bit value for the low threshold also reset the given threshold
+    
+    unsigned char wbuf[1];
+    uint8_t data[2];
+    wbuf[0] = DS1631_SET_TH;
+    data[0] = temp;
+    data[1] = 0;
+    // set high threshold
+    i2c_io(DS1631_ID, wbuf, 1, data, 2, NULL , 0);
+  
 }
 
-unsigned char ds1631_conversion_status() {
+void ds1631ReadTH(unsigned char* th_buf)
+{
+    unsigned char wbuf[1];
+    wbuf[0] = DS1631_SET_TH;
+    i2c_io(DS1631_ID, wbuf, 1, NULL, 0, th_buf , 2);
+    
+ 
+}
+
+unsigned char ds1631_conversion_status()
+{
     unsigned char rbuf;
     unsigned char wbuf[1] = {DS1631_ACCESS_CONFIG};
     i2c_io(DS1631_ID, wbuf, 1, NULL, 0, &rbuf, 1); // Read sensor config into rbuf
 
-    if(rbuf & 0x80) { //Check MSB of config register
+    if (rbuf & 0x80)
+    { // Check MSB of config register
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
@@ -123,16 +130,17 @@ unsigned char ds1631_conversion_status() {
 /*
     Read temp data (2 bytes) into buf
 */
-void ds1631_read_temp(unsigned char* rbuf) {
+void ds1631_read_temp(unsigned char *rbuf)
+{
     unsigned char config = ds1631_get_config();
     // //int temperature;
     // //unsigned char temperature;
-    if (config & 0x01) { // If we are in one-shot mode then wait for conversion, otherwise read now
-        while (!ds1631_conversion_status()) {
+    if (config & 0x01)
+    { // If we are in one-shot mode then wait for conversion, otherwise read now
+        while (!ds1631_conversion_status())
+        {
             // Wait for conversion to complete
-        }                                                                         
+        }
     }
-    ds1631_read_command(DS1631_READ_TEMP, rbuf); 
-    
-    
+    ds1631_read_command(DS1631_READ_TEMP, rbuf);
 }
